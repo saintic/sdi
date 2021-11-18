7a58ea22b5eb40a96b561e53ed555cec2552740a
Hiroshi.tao <me@tcw.im>
2021-11-18 22:23:13 +0800

    chore: new homepage

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
+/*
+   Copyright 2021 Hiroshi.tao
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package main
 
 import (
 	"embed"
-	"html/template"
+	"io/fs"
 	"net/http"
 )
 
 //go:embed _/bjfu
 var bjfu embed.FS
 
-//go:embed index.html
-var indexTmpl []byte
+//go:embed dist
+var dist embed.FS
+var html, _ = fs.Sub(dist, "dist")
 
 func main() {
 	http.Handle("/_/", http.FileServer(http.FS(bjfu)))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.New("index").Parse(string(indexTmpl))
-		tmpl.Execute(w, nil)
-	})
+	http.Handle("/", http.FileServer(http.FS(html)))
 	http.ListenAndServe(":12345", nil)
 }
